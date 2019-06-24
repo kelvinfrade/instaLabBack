cb6abb793f56f0133bae7180bacbfcbc23794834
Kelvin Frade <kelvinfrade@gmail.com>
2019-06-24 15:18:31 -0300

    add variaveis ambientes

--- src/models/Post.js
+++ src/models/Post.js
@@ -12,6 +12,14 @@ const PostSchema = new mongoose.Schema({
     }
 }, {
     timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
+});
+
+PostSchema.virtual("url").get(function() {
+    const url = process.env.URL || 'http://localhost:3333'
+
+    return `${url}/files/${encodeURIComponent(this.path)}`;
 });
 
 module.exports = mongoose.model('Post', PostSchema);
