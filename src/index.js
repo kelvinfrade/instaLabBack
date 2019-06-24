09af0f3ecf08a78713a6c126cad216c10c70ff1e
Kelvin Frade <kelvinfrade@gmail.com>
2019-06-24 11:56:39 -0300

    Procfile

--- src/index.js
+++ src/index.js
@@ -19,9 +19,12 @@ app.use((req, res, next) => {
 
 app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 app.use('./files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require("./routes"));
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
 
