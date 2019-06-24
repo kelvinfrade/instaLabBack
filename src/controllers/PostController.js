a2e9afb3927a121fa5df8562e321c063a685b778
Kelvin Frade <kelvinfrade@gmail.com>
2019-06-24 13:20:06 -0300

    Delete Dependence Sharp

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,7 +1,7 @@
 const Post = require('../models/Post');
-const sharp = require('sharp');
+//const sharp = require('sharp');
 const path = require('path');
-const fs = require('fs');
+//const fs = require('fs');
 
 module.exports = {
     async index(req, res) {
@@ -17,14 +17,14 @@ module.exports = {
         const [name] = image.split('.');
         const fileName = `${name}.jpg`;
 
-        await sharp(req.file.path)
-            .resize(500)
-            .jpeg({ quality: 70 })
-            .toFile(
-                path.resolve(req.file.destination, 'resized', fileName)
-            )
+        //await sharp(req.file.path)
+          //  .resize(500)
+            //.jpeg({ quality: 70 })
+            //.toFile(
+              //  path.resolve(req.file.destination, 'resized', fileName)
+            //)
 
-        fs.unlinkSync(req.file.path);
+        //fs.unlinkSync(req.file.path);
 
         const post = await Post.create({
             author,
