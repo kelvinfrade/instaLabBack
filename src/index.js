fc48918d940b6d8ceb3db3a460f16f63429741de
Kelvin Frade <kelvinfrade@gmail.com>
2019-06-24 14:35:18 -0300

    gitkeep

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 const server = require('http').Server(app);
 const io = require('socket.io')(server)
 
@@ -17,7 +19,7 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use(cors());
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
